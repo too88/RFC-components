69422b27aa9c4ccf529f43f2df77774ec8a6990a
toohuynh <tu.huynh@4digit.jp>
2024-12-26 10:11:22 +0700

    chore: fix Global declare file for vite

---
+++ src/Global.d.ts
@@ -0,0 +1,6 @@
+// Files with below extensions are allows the import
+declare module "*.module.css";
+declare module "*.module.scss";
+declare module "*.png";
+declare module "*.svg";
+declare module "*.jpg";
