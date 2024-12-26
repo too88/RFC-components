6a382831df943b0e83762685a7a5c0b93cd726d6
toohuynh <tu.huynh@4digit.jp>
2024-12-26 08:49:38 +0700

    chore: update eslint-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,41 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import eslintPluginPrettier from 'eslint-plugin-prettier'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js";
+import globals from "globals";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  { ignores: ['dist', 'vite.config.ts'] },
+  { ignores: ["dist", "vite.config.ts"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser
+      globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-      prettier: eslintPluginPrettier
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }]
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
-    'prettier/prettier': [
-      'warn',
+    "prettier/prettier": [
+      "warn",
       {
-        arrowParens: 'always',
-        semi: false,
-        trailingComma: 'none',
+        arrowParens: "avoid",
+        endOfLine: "crlf",
+        trailingComma: "es5",
+        bracketSpacing: true,
         tabWidth: 2,
-        endOfLine: 'auto',
-        useTabs: false,
         singleQuote: true,
-        printWidth: 120,
-        jsxSingleQuote: true
-      }
-    ]
+        printWidth: 350,
+      },
+    ],
   }
-)
+);
