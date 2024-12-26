094e6ea3ea1d48fdde203bec715e74c004fc086a
toohuynh <tu.huynh@4digit.jp>
2024-12-27 02:00:22 +0700

    chore: update prettier format code

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import { Col, Flex, Form, Row } from "antd";
+import React from 'react';
+import { Col, Flex, Form, Row } from 'antd';
 
-import Hero from "~/components/organisms/hero";
-import markerPlaceStyle from "./index.module.scss";
-import Category from "~/components/molecules/category";
-import useMarketplaceActions from "~/hooks/useMarketplaceActions";
-import SearchComponent, {
-  FieldType,
-} from "~/components/molecules/filter/index.tsx";
-import CardList from "~/components/molecules/card-list/index.tsx";
+import Hero from '~/components/organisms/hero';
+import markerPlaceStyle from './index.module.scss';
+import Category from '~/components/molecules/category';
+import useMarketplaceActions from '~/hooks/useMarketplaceActions';
+import SearchComponent, { FieldType } from '~/components/molecules/filter/index.tsx';
+import CardList from '~/components/molecules/card-list/index.tsx';
 
 const Marketplace = () => {
   const [form] = Form.useForm<FieldType>();
-  const { addMoreDataFn, getProductList, data, loading, isLoading } =
-    useMarketplaceActions(form);
+  const { addMoreDataFn, getProductList, data, loading, isLoading } = useMarketplaceActions(form);
 
   return (
     <React.Fragment>
@@ -21,23 +18,14 @@ const Marketplace = () => {
 
       <Row className={markerPlaceStyle.container}>
         <Col xs={24} md={24} lg={6} xl={5}>
-          <SearchComponent
-            form={form}
-            loading={loading}
-            getListFn={getProductList}
-          />
+          <SearchComponent form={form} loading={loading} getListFn={getProductList} />
         </Col>
 
         <Col xs={24} md={24} lg={16} xl={18}>
           <Flex gap={40} vertical>
             <Category getListFn={getProductList} />
 
-            <CardList
-              data={data}
-              loadMoreFn={addMoreDataFn}
-              loading={loading}
-              isLoading={isLoading}
-            />
+            <CardList data={data} loadMoreFn={addMoreDataFn} loading={loading} isLoading={isLoading} />
           </Flex>
         </Col>
       </Row>
@@ -46,3 +34,4 @@ const Marketplace = () => {
 };
 
 export default Marketplace;
+
